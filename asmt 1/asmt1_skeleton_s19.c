73ab1ee01aebff62b175a8923b028d914498969e
nh916 <navidhariri25@gmail.com>
2019-03-17 16:58:16 -0700

    Add files via upload

---
+++ asmt 1/asmt1_skeleton_s19.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* declare global var's, including dynamic array of integers for
+CPI of classes of instructions and Instruction count */
+
+
+/*********************************************************/
+void "OPTION #1"()
+{
+/* declare local var's and initialize*/
+
+/* prompt for # instruction classes & frequency of machine */
+
+/* for each instruction class, prompt for CPI of class and instruction count */
+/* store values in appropriate dynamic arrays */
+
+return;
+}
+
+/*********************************************************/
+void "OPTION #2"()
+{
+/* declare local var's */
+
+/* Print table of parameters, one row per class via a for-loop*/
+  
+return;
+}
+
+/*********************************************************/
+void "OPTION #3"()
+{
+/* declare local var's */
+/* calculate CPI average */
+/* calculate total execution time */
+/* calculate MIPS */
+
+/* Print table of results */
+
+return;
+}
+
+
+/*********************************************************/
+int main()
+{
+/* declare local var's */
+
+/* until user chooses to quit, print menu, 
+select choice via switch statement and call appropriate function*/
+
+return 1;
+}
